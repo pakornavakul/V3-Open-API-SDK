af1e43e25f3f1b70a65b4af2d800bdd33ec1d6e4
pakornavakul <chrisavakul@gmail.com>
2021-04-16 10:43:25 +0700

    my_edit

---
+++ okex-python-sdk-api/okex/api_v5.py
@@ -0,0 +1,8 @@
+from .client import Client
+from .consts import *
+
+
+class SpotAPI(Client):
+
+    def __init__(self, api_key, api_secret_key, passphrase, use_server_time=False, test=False, first=False):
+        Client.__init__(self, api_key, api_secret_key, passphrase, use_server_time, test, first)
